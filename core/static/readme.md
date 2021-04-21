6a25caf34e7cb63731633e98965d499cbc49b6fb
Alan Oliveira <alancjobm@gmail.com>
2021-04-21 11:55:57 -0300

    adicionando arquivos estaticos

---
+++ core/static/readme.md
@@ -0,0 +1,1 @@
+Aqui é onde o Django busca os arquivos estáticos 
--- discografia/settings.py
+++ discografia/settings.py
@@ -123,6 +123,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# this is the static files folder name which you created in django project root folder. This is different from above STATIC_URL. 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, '/../core/statics'),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
