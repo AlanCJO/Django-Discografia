f28b69186c31d842fc96527c0695d462b9a0c0c3
Alan Oliveira <alancjobm@gmail.com>
2021-04-21 20:51:19 -0300

    fet: editar musica implementado

--- disco/urls.py
+++ disco/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import getAllMusics, musica_new
+from .views import getAllMusics, musica_new, musica_delete, musica_edit
 
 app_name = 'disco'
 
 urlpatterns = [
     path('musica-list/', getAllMusics, name='musicas'),
-    path('nova-musica/', musica_new, name="nova-musica") 
+    path('nova-musica/', musica_new, name="nova-musica"),
+    path('editar-musica/<int:pk>/', musica_edit, name="editar-musica")
 ]
--- disco/views.py
+++ disco/views.py
@@ -15,11 +15,12 @@ def getAllMusics(request):
 
     return render(request, template_name, context)
 
+
 def musica_new(request):
     if request.method == 'POST':
         form = MusicaForm(request.POST)
         if form.is_valid():
-            form.save
+            form.save()
             return redirect('disco:musicas')
     else:
         template_name = 'nova_musica.html'
@@ -27,3 +28,23 @@ def musica_new(request):
             'form': MusicaForm()
         }
         return render(request, template_name, context)
+
+
+def musica_edit(request, pk):
+    musica = Musica.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = MusicaForm(request.POST, instance=musica)
+        if form.is_valid():
+            form.save()
+            return redirect('disco:musicas')
+    else:
+        template_name = "editar_musica.html"
+        context = {
+            'form': MusicaForm(instance=musica),
+            'pk': pk 
+        }
+        return render(request, template_name, context)
+
+
+def musica_delete(request, pk):
+    pass
