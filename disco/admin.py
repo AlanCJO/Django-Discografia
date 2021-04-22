2847c58692474fda2e6eced472bd84429a0e7593
Alan Oliveira <alancjobm@gmail.com>
2021-04-22 00:46:26 -0300

    projeto pronto para entrega!

--- disco/admin.py
+++ disco/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from .models import *
 
+class MusicasAdmin(admin.ModelAdmin):
+    list_display = ['titulo', 'segundos', 'album']
 
+
+admin.site.register(Musica, MusicasAdmin)
 admin.site.register(Banda)
 admin.site.register(Album)
-admin.site.register(Musica)
